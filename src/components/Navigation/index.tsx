1663193c0494763d7e378f2e4128b1c501499221
liujingmin <1220839184@qq.com>
2025-01-14 18:00:40 +0800

    nav基本样式

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,23 +4,28 @@ import {LogoIcon} from "@/components/LogoIcon";
 import {useRouter} from "next/navigation";
 import React from "react";
 import {Profile} from "@/components/Profile";
+import classNames from "classnames";
 
 const Navigation = () => {
     const router = useRouter();
 
     return <div className={styles['nav-container']}>
-        <span
-            className={styles.logo}
-            onClick={() => {
-                router.push('/');
-            }}
-        >
-            <LogoIcon />
-            创见
+        <span className={styles['nav-left']}>
+            <span
+                className={classNames(styles.logo)}
+                onClick={() => {
+                    router.push('/');
+                }}
+            >
+                <LogoIcon/>
+                创见
+            </span>
         </span>
-        <Input.Search className={styles.search}/>
-        <Button>创作</Button>
-        <Profile />
+        <span className={styles['nav-right']}>
+            <Input.Search className={styles.search} placeholder='搜索'/>
+            <Button>创作</Button>
+            <Profile/>
+        </span>
     </div>
 }
 
