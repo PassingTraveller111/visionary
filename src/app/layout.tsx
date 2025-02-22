37d91b568c019b2bd468e4bc50458942b47388e6
liujingmin <1220839184@qq.com>
2025-02-22 21:01:21 +0800

    支持latex公式

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import 'highlight.js/styles/default.css'; // 引入代码高亮样式
+import 'katex/dist/katex.min.css'; // 引入 KaTeX 样式
 import {ReduxProvider} from "@/store/provider";
 import ClientEntry from "@/components/ClientEntry";
 
--- src/components/ReactMarkdown/index.tsx
+++ src/components/ReactMarkdown/index.tsx
@@ -1,17 +1,19 @@
 import ReactMarkdown from 'react-markdown';
 import styles from './index.module.scss';
 import rehypeHighlight from "rehype-highlight";
-import 'highlight.js/styles/default.css'; // 引入高亮样式
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 const MyReactMarkdown = (props) => {
     return <span className={styles.markdown}>
-        <ReactMarkdown
-            rehypePlugins={[rehypeHighlight]}
-            {...props}
-        />
+            <ReactMarkdown
+                remarkPlugins={[remarkMath]}
+                rehypePlugins={[rehypeHighlight, rehypeKatex]}
+                {...props}
+            />
     </span>
-}
+};
 
 export default MyReactMarkdown;
