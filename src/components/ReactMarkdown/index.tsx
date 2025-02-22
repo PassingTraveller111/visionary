cd09ad6f3f826af072da7d06aa0c133e4a160f8b
liujingmin <1220839184@qq.com>
2025-02-22 20:14:27 +0800

    添加了代码高亮

--- src/components/ReactMarkdown/index.tsx
+++ src/components/ReactMarkdown/index.tsx
@@ -1,11 +1,16 @@
 import ReactMarkdown from 'react-markdown';
 import styles from './index.module.scss';
+import rehypeHighlight from "rehype-highlight";
+import 'highlight.js/styles/default.css'; // 引入高亮样式
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 const MyReactMarkdown = (props) => {
     return <span className={styles.markdown}>
-        <ReactMarkdown {...props}/>
+        <ReactMarkdown
+            rehypePlugins={[rehypeHighlight]}
+            {...props}
+        />
     </span>
 }
 
