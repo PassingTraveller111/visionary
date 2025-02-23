f074d9065f8e6f7053903a06116623aaa3a7ec8c
liujingmin <1220839184@qq.com>
2025-02-23 11:19:03 +0800

    支持表格渲染

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,20 @@ const nextConfig: NextConfig = {
     eslint: {
         // 构建时禁用检查
         ignoreDuringBuilds: true,
-    }
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*", // 匹配所有 API 路径
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // 替换为你的实际 Origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
--- src/components/ReactMarkdown/index.tsx
+++ src/components/ReactMarkdown/index.tsx
@@ -3,13 +3,14 @@ import styles from './index.module.scss';
 import rehypeHighlight from "rehype-highlight";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import remarkGfm from "remark-gfm";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 const MyReactMarkdown = (props) => {
     return <span className={styles.markdown}>
             <ReactMarkdown
-                remarkPlugins={[remarkMath]}
+                remarkPlugins={[remarkMath, remarkGfm]}
                 rehypePlugins={[rehypeHighlight, rehypeKatex]}
                 {...props}
             />
