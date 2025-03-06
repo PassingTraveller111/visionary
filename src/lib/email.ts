d07c5bfb86b35633d9f4ffcec6875f7aa06f932a
liujingmin <1220839184@qq.com>
2025-03-06 17:37:08 +0800

    邮件配置修改

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -2,8 +2,11 @@ import nodemailer from 'nodemailer';
 
 // 创建一个 SMTP 传输对象
 const transporter = nodemailer.createTransport({
-    // 使用 QQ 作为邮件服务提供商
+    // 使用 163 作为邮件服务提供商
     service: process.env.EMAIL_SERVICE,
+    host: 'smtp.163.com', // 修改为 163 邮箱的 SMTP 服务器地址
+    port: 465, // 163 邮箱使用 465 端口（SSL 加密）
+    secure: true, // 使用 SSL 加密
     auth: {
         // 发件人的邮箱地址
         user: process.env.EMAIL_USER,
