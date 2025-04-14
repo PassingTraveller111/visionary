d4254a8f19be801738d1900fdecc1fc5f90fe8fd
liujingmin <1220839184@qq.com>
2025-04-14 20:10:19 +0800

    toolbar添加默认的pluginconfig

--- src/components/Diagram/DiagramHeader/Plugins/RootPlugins.ts
+++ src/components/Diagram/DiagramHeader/Plugins/RootPlugins.ts
@@ -1,8 +1,20 @@
 import TextAlignPlugin from "@/components/Diagram/DiagramHeader/Plugins/TextAlignPlugin";
+import {configType} from "@/components/Diagram/DiagramHeader/Plugins/type";
 
 
 const rootPlugins = [
     TextAlignPlugin
 ];
 
+const defaultPluginConfig: configType = {
+    name: '',
+    align: 'left'
+}
+
+rootPlugins.map(Plugin => {
+    return Plugin.config = {
+        ...defaultPluginConfig,
+        ...Plugin.config,
+    }
+})
 export default rootPlugins;
--- src/components/Diagram/DiagramHeader/index.tsx
+++ src/components/Diagram/DiagramHeader/index.tsx
@@ -17,7 +17,7 @@ const DiagramHeader = () => {
             <div className={styles.toolbarLeft}>
                 {
                     RootPlugins
-                        .filter(Plugin => Plugin.config.align !== 'right')
+                        .filter(Plugin => Plugin.config.align === 'left')
                         .map((Plugin) => {
                             return <Plugin key={Plugin.name}/>
                         })
