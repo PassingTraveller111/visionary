d788ff3bee5a99a3759c711bc2c1be0bc7b4600c
liujingmin <1220839184@qq.com>
2025-06-09 11:16:06 +0800

    新增几种流程图的图形

--- src/components/Diagram/Nodes/FlowNode/shapes.ts
+++ src/components/Diagram/Nodes/FlowNode/shapes.ts
@@ -26,13 +26,13 @@ export const Shapes: {
         },
         Triangle: {
             d: 'M50,0 L100,100 L0,100 Z',
-            title: '矩形',
+            title: '三角形',
             width: 120,
             height: 60,
         },
         Rectangle: {
             d: 'M0,0 V100 H100 V0 Z',
-            title: '三角形',
+            title: '矩形',
             width: 120,
             height: 60,
         },
@@ -50,6 +50,48 @@ export const Shapes: {
             title: '开始/结束',
             width: 120,
             height: 60,
-        }
+        },
+        Flow: {
+            d: 'M0,0 V100 H100 V0 Z',
+            title: '流程',
+            width: 120,
+            height: 60,
+        },
+        Judge: {
+            d: 'M50,0 L100,50 L50,100 L0,50 Z',
+            title: '判定',
+            width: 120,
+            height: 60,
+        },
+        Document: {
+            d: 'M0,0 L0,90 Q25,110 50,90 Q75,80 100,90 L100,0 Z',
+            title: '文档',
+            width: 120,
+            height: 60,
+        },
+        Data: {
+            d: 'M40,0 L100,0 L60,100 L0,100 Z',
+            title: '数据',
+            width: 120,
+            height: 60,
+        },
+        ChildFlow: {
+            d: 'M0,0 L0,100 L100,100 L100,0 L20,0 L20,100 L80,100 L80,0 Z',
+            title: '子流程',
+            width: 120,
+            height: 60,
+        },
+        outerData: {
+            d: 'M10,0 Q-10,50 10,100 L100,100 Q90,50 100,0 Z',
+            title: '外部数据',
+            width: 120,
+            height: 60,
+        },
+        DataBase: {
+            d: 'M90,0 Q80,50 90,100 Q100,50 90,0 L10,0 Q0,50 10,100 L90,100',
+            title: '数据库',
+            width: 120,
+            height: 60,
+        },
     }
 }
